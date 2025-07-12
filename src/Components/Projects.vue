f627199d9c03edbdeabc08f2ed7efaa468be69a2
yashwantreddy103 <yashwantreddy65@gmail.com>
2025-07-12 19:21:52 +0530

    Create Projects.vue

---
+++ src/Components/Projects.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="projects-section">
+    <h2>Projects</h2>
+    <div class="no-projects">
+      <p>
+        I am currently focused on learning and exploring cyber security.<br>
+        Projects will be added here soon!
+      </p>
+    </div>
+  </section>
+</template>
